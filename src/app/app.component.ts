4137e0096b40d7baeaf88f050244b54efa7bda3e
deduc <deductivegoose@outlook.es>
2023-12-18 17:56:21 +0100

    new app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { appName } from './core/constants/constants';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'spotify-clon';
+    public title: string = appName;
 }
