7a64cdb95fe3c7b81f7be0e30f4fbdead9ed24c6
deduc <deductivegoose@outlook.es>
2023-12-18 17:53:23 +0100

    nuevo fichero constantes

---
+++ src/app/core/constants/constants.ts
@@ -0,0 +1,11 @@
+export const appName: string = "SoniFy";
+export const githubLink: string = "https://github.com/deduc";
+export const email: string = "deductivegoose@outlook.es";
+
+// * Spotify urls
+export const SpotifyAuthUrl: URL = new URL("https://accounts.spotify.com/authorize");
+export const SpotifyTokenUrl: string = "https://accounts.spotify.com/api/token";
+
+// * API endpoints
+export const APIMakeAuthParams: string = "https://localhost:7293/auth/getAuthParams";
+export const ApiGetClientSecret: string = "https://localhost:7293/auth/getClientSecret"
