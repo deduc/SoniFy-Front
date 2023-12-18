d7ea5e1e17b3514cc9bcf2b09d47845e17c8efd5
deduc <deductivegoose@outlook.es>
2023-12-18 17:56:28 +0100

    nuevos modulos añadidos

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SharedModule } from '../shared/shared.module';
-
+// mis componentes
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MyPlaylistsComponent } from './my-playlists/my-playlists.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
+
+// mis modulos
+import { SharedModule } from '../shared/shared.module';
+// modulo http para usar el componente HttpClient
+import { HttpClientModule } from '@angular/common/http';
+import { LoginService } from '../pages/login/login.service';
 
 
 @NgModule({
@@ -23,6 +28,10 @@ import { MyProfileComponent } from './my-profile/my-profile.component';
     CommonModule,
     SharedModule,
     PagesRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    LoginService
   ]
 })
 export class PagesModule { }
