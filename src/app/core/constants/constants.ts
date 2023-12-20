ec2a57ba3b3059cf7f7361d558fdd1e889f265fc
deduc <deductivegoose@outlook.es>
2023-12-20 13:46:06 +0100

    reestructuracion constants

--- src/app/core/constants/constants.ts
+++ src/app/core/constants/constants.ts
@@ -1,11 +1,20 @@
-export const appName: string = "SoniFy";
-export const githubLink: string = "https://github.com/deduc";
-export const email: string = "deductivegoose@outlook.es";
-
-// * Spotify urls
+//* Spotify urls
 export const SpotifyAuthUrl: URL = new URL("https://accounts.spotify.com/authorize");
 export const SpotifyTokenUrl: string = "https://accounts.spotify.com/api/token";
 
-// * API endpoints
-export const APIMakeAuthParams: string = "https://localhost:7293/auth/getAuthParams";
-export const ApiGetClientSecret: string = "https://localhost:7293/auth/getClientSecret"
+//* API endpoints
+export const APIUrl: string = "https://localhost";
+export const APIPort: string = "7293"
+export const APIBaseUrl: string = `${APIUrl}:${APIPort}/`;
+export const APIMakeAuthParams: string = `${APIBaseUrl}auth/getAuthParams`;
+export const ApiGetClientSecret: string = `${APIBaseUrl}auth/getClientSecret`;
+
+// LocalStorage keys
+export const accessTokenKey: string = "access_token";
+export const clientIdKey: string = "clientId";
+export const clientSecretKey: string = "clientSecret";
+
+// Otras variables
+export const appName: string = "SoniFy";
+export const githubLink: string = "https://github.com/deduc";
+export const email: string = "deductivegoose@outlook.es";
