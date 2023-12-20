c233b1fff4cffa9b6053f14110888f0c3a2ec02a
deduc <deductivegoose@outlook.es>
2023-12-20 13:47:36 +0100

    cambios en logincomponent

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { appName } from 'src/app/core/constants/constants';
+import { ApiGetClientSecret, SpotifyAuthUrl, SpotifyTokenUrl, appName } from 'src/app/core/constants/constants';
 import { LoginService } from './login.service';
 import { AuthParamsInterface } from './interfaces/AuthParamsInterface';
+import { ClientSecretInterface } from './interfaces/ClientSecretInterface';
 
 
 @Component({
@@ -16,8 +17,23 @@ export class LoginComponent{
     
     constructor(private http: HttpClient, private loginService: LoginService) { }
     
-
     public async login(){
-        const authParams: AuthParamsInterface = await this.loginService.getAuthParams();
+        try {
+            const authParams: AuthParamsInterface = await this.loginService.getAuthParams();
+            const clientId: string = authParams.client_id;
+            
+            const clientSecret: string = await this.loginService.getClientSecret(ApiGetClientSecret);
+            
+
+            localStorage.setItem("clientId", clientId);
+            localStorage.setItem("clientSecret", clientSecret);
+            
+            SpotifyAuthUrl.search = new URLSearchParams({...authParams}).toString();
+            window.location.href = SpotifyAuthUrl.toString();
+        }
+        catch (err){
+            console.log(err);
+            
+        }
     }
 }
