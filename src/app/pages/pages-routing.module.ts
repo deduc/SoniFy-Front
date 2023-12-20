1e5247a29cd3a986e57778f5bfdef2b34d4f1718
deduc <deductivegoose@outlook.es>
2023-12-20 13:46:30 +0100

    authservice added

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -5,10 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { MyPlaylistsComponent } from './my-playlists/my-playlists.component';
 
+import { AuthService } from '../core/auth.service';
+
+
 const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'my-playlists', component: MyPlaylistsComponent },
-    { path: 'my-profile', component: MyProfileComponent },
+    { path: 'home', component: HomeComponent, canActivate: [AuthService] },
+    { path: 'my-playlists', component: MyPlaylistsComponent, canActivate: [AuthService] },
+    { path: 'my-profile', component: MyProfileComponent, canActivate: [AuthService] },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
