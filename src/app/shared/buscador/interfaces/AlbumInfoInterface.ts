904ec5bc8c3a17f2e3ed1f8fb2c939f49fca17fe
deduc <deductivegoose@outlook.es>
2023-12-21 20:42:04 +0100

    cambios pochos y nuevos ficheros

---
+++ src/app/shared/buscador/interfaces/AlbumInfoInterface.ts
@@ -0,0 +1,10 @@
+export interface AlbumInfoInterface {
+    album_type: string;
+    img_url: string;
+    name: string;
+    release_date: string;
+    spotify_url: string;
+    total_tracks: string;
+    api_href: string;
+    api_id: string;
+}
