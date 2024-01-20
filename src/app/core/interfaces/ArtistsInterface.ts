c19a6afb2bdc32776ef8045b2a47a08056dd14c1
deduc <deductivegoose@outlook.es>
2024-01-20 12:39:21 +0100

    comentarios añadidos

--- src/app/core/interfaces/ArtistsInterface.ts
+++ src/app/core/interfaces/ArtistsInterface.ts
@@ -1,7 +1,12 @@
 export interface ArtistInterface {
+    // Enlace del artista en spotify
     external_url: string,
-    genres: string,
+    // Lista de géneros musicales
+    genres: string[],
+    // Sirve para hacer peticiones a la api para obtener información más detallada sobre el artista
     id: string,
+    // Imagen
     image_url: string,
+    // Nombre del artista
     name: string,
 }
--- src/app/core/interfaces/TopArtistListInterface.ts
+++ src/app/core/interfaces/TopArtistListInterface.ts
@@ -1,9 +1,14 @@
 import { ArtistInterface } from "./ArtistsInterface";
 
 export interface TopArtistListInterface {
+    // Link al endpoint de Spotify
     href: string,
+    // Indica la cantidad máxima de objetos ArtistInterface que va a obtener
     limit: number,
+    // Endpoint para obtener los próximos objetos ArtistInterface
     next: string | null,
+    // Cantidad total de artistas que se pueden obtener
     total: number,
+    // Lista de artistas
     items: ArtistInterface[],
 }
