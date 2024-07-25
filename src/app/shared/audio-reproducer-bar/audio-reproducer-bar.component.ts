9bbbbd843ea823bcd95090a3b64a8a4b740608f5
deduc <deductivegoose@outlook.es>
2024-07-25 20:44:06 +0200

    backup audioReproducerBarComponent

--- src/app/shared/audio-reproducer-bar/audio-reproducer-bar.component.ts
+++ src/app/shared/audio-reproducer-bar/audio-reproducer-bar.component.ts
@@ -1,6 +1,8 @@
 // src/app/shared/audio-reproducer-bar/audio-reproducer-bar.component.ts
 import { Component, OnInit } from '@angular/core';
-import { accessTokenKey } from '../../core/constants/constants';
+import { accessTokenKey, spotifySdkPlayerName } from '../../core/constants/constants';
+import { SpotifySdkWebPlayerService } from 'src/app/core/global-services/spotify-sdk-web-player.service';
+import { MediaPlayerDevice } from './interfaces/MediaPlayerDevice.interface';
 
 
 @Component({
@@ -9,73 +11,75 @@ import { accessTokenKey } from '../../core/constants/constants';
     styleUrls: ['./audio-reproducer-bar.component.css']
 })
 export class AudioReproducerBarComponent implements OnInit {
-    ngOnInit(): void {}
+    public idDevice: string = "";
+    public mediaPlayerDevices: MediaPlayerDevice[] = [];
+    public spotifySdkPlayerName: string = spotifySdkPlayerName;
+    public trackUri: string = "";
+    
+    private spotifyService: SpotifySdkWebPlayerService;
+    private sonifyMediaName: string = spotifySdkPlayerName;
+    private lastPlayedTrackUri: string = "";
 
-    play(){}
-    pause(){}
+    constructor(spotifyService: SpotifySdkWebPlayerService) {
+        this.spotifyService = spotifyService;
+        
+        setTimeout(async () => {
+            this.loadMediaPlayerDevices();
+            this.lastPlayedTrackUri = await this.spotifyService.getLastPlayedTrack();
+        }, 2000);
+    }
+
+    ngOnInit(): void { }
+
+    onDeviceChange(event: Event): void {
+        const selectElement = event.target as HTMLSelectElement;
+        // resto 1 para evitar seleccionar la opción "Selecciona un dispositivo"
+        const selectedIndex = selectElement.selectedIndex - 1;
+
+        if (selectedIndex >= 0) {
+            const selectedItem: MediaPlayerDevice = this.mediaPlayerDevices[selectedIndex];
+            this.idDevice = selectedItem.id;
+        }
+    }
+
+    public play(): void {
+        let id: string = this.idDevice;
+
+        if (id != null) {
+            this.spotifyService.playOnThisDevice(this.trackUri, id);
+        }
+        else if (this.idDevice != null) {
+            this.spotifyService.playOnThisDevice(this.trackUri, this.idDevice);
+        }
+        else {
+            console.error("ERROR: No se ha podido obtener el ID del reproductor de sonido.");
+        }
+
+        this.spotifyService.play();
+    }
+
+    public pause(): void {
+        this.spotifyService.pause();
+    }
+
+    public nextTrack(): void {
+        this.spotifyService.nextTrack();
+    }
+
+    public previousTrack(): void {
+        this.spotifyService.previousTrack();
+    }
+
+    // * privados
+    // * privados
+    // * privados
+
+    private loadMediaPlayerDevices(): void {
+        this.spotifyService.mediaPlayerDevices$.subscribe(
+            (apiRes: any) => {
+                let mediaPlayersAux: MediaPlayerDevice[] = apiRes;
+                this.mediaPlayerDevices = mediaPlayersAux;
+            }
+        )
+    }
 }
-// export class AudioReproducerBarComponent implements OnInit {
-//     public player: Spotify.Player | any;
-//     public accessToken: string;
-//     private accessTokenKey: string = accessTokenKey;
-
-//     constructor() {
-//         this.accessToken = localStorage.getItem(this.accessTokenKey)!;
-//     }
-
-//     ngOnInit() {
-//         this.loadSpotifySDK();
-//     }
-
-//     loadSpotifySDK() {
-//         const script = document.createElement('script');
-//         script.src = 'https://sdk.scdn.co/spotify-player.js';
-//         document.body.appendChild(script);
-//         script.onload = () => this.initializeSpotifyPlayer();
-//     }
-
-//     initializeSpotifyPlayer() {
-//         console.log(this.player.connect());
-
-
-//         this.player = new Spotify.Player({
-//             name: 'Web Playback SDK Quick Start Player',
-//             getOAuthToken: (cb: (token: string) => void) => { cb(this.accessToken!); },
-//             volume: 1
-//         });
-
-//         this.player.connect();
-
-//         // Ready
-//         this.player.addListener('ready', ({ device_id }: { device_id: string }) => {
-//             console.log('Ready with Device ID', device_id);
-//         });
-
-//         // Not Ready
-//         this.player.addListener('not_ready', ({ device_id }: { device_id: string }) => {
-//             console.log('Device ID has gone offline', device_id);
-//         });
-//     }
-
-//     play() {
-//         if (this.player) {
-//             this.player.resume().then(() => {
-//                 console.log('Playback resumed');
-//             });
-//         }
-//         else {
-//             console.error("No existe this.player")
-//         }
-//     }
-
-//     pause() {
-//         if (this.player) {
-//             this.player.pause().then(() => {
-//                 console.log('Playback paused');
-//             });
-//         }
-//         else {
-//             console.error("No existe this.player")
-//         }
-//     }
-// }
