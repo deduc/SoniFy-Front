2167209f9c74155f50c589f95da9149c9a368044
deduc <deductivegoose@outlook.es>
2024-07-28 03:14:13 +0200

    nuevas interfaces creadas

---
+++ src/app/core/interfaces/TrackInfoInterface.ts
@@ -0,0 +1,11 @@
+export interface TrackInfoInterface {
+    apiHref: string,
+    externalUrl: string,
+    group: string,
+    time: string,
+    tittle: string,
+    idTrack: string,
+    imgUrl: string,
+    uri: string,
+    popularity: string | null,
+}
