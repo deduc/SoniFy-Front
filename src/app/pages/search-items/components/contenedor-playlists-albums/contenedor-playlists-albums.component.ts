23eab652e853ce417270816a11ab4fcb87819b2b
deduc <deductivegoose@outlook.es>
2024-08-02 19:50:55 +0200

    botones cambian de color en funcion de quién lo pulsa

--- src/app/pages/search-items/components/contenedor-playlists-albums/contenedor-playlists-albums.component.ts
+++ src/app/pages/search-items/components/contenedor-playlists-albums/contenedor-playlists-albums.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, } from '@angular/core';
+
 import { AlbumDataInterface } from 'src/app/core/interfaces/AlbumDataInterface';
 import { PlaylistDataInterface } from 'src/app/core/interfaces/PlaylistsDataInterface';
 
@@ -12,12 +13,31 @@ export class ContenedorPlaylistsAlbumsComponent {
     public albums: AlbumDataInterface[] = [];
     @Input()
     public playlists: PlaylistDataInterface[] = [];
-    
+
     public componentIndexToRender: number = 0;
+
+    private cssClassButtonPressed: string = "button-pressed";
+    private cssClassButtonNonPressed: string = "button-non-pressed";
+
+    public cssClassBtn0: string = this.cssClassButtonPressed;
+    public cssClassBtn1: string = this.cssClassButtonNonPressed;
+
+
+
 
     constructor() { }
 
+
     public changeComponentToRender(index: number): void {
         this.componentIndexToRender = index;
+
+        if (index == 0) {
+            this.cssClassBtn0 = this.cssClassButtonPressed;
+            this.cssClassBtn1 = this.cssClassButtonNonPressed;
+        }
+        else if (index == 1) {
+            this.cssClassBtn1 = this.cssClassButtonPressed;
+            this.cssClassBtn0 = this.cssClassButtonNonPressed;
+        }
     }
 }
